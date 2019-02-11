8c6381924c07c4392072eec595e71c9380a8c48f
mdtrotter <mtrotter@aggies.ncat.edu>
2019-02-10 21:16:30 -0500

    fix return statement

--- app/routes.py
+++ app/routes.py
@@ -19,8 +19,8 @@ def login():
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
-        return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+            return redirect(next_page)
+        return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/logout')
 def logout():
