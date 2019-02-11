362d30a19ca1e8be2c4794de21d803e6f69fcdc3
mdtrotter <mtrotter@aggies.ncat.edu>
2019-02-10 21:38:30 -0500

    fix routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from app import app, db
+from app import db
 from app.forms import LoginForm, RegistrationForm
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
